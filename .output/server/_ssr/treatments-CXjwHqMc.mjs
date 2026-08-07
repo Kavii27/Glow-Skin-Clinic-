dcab92441b99ba422894c66eaf78e16f3c372d74
Kaveesha Sandeepani <kaveeshasandeepani027@gmail.com>
2026-08-07 15:41:15 +0530

    removed unnecessary files from the repo

--- .output/server/_ssr/treatments-CXjwHqMc.mjs
+++
@@ -1,9 +0,0 @@
-import { _ as require_jsx_runtime } from "../_libs/@radix-ui/react-accordion+[...].mjs";
-import { f as Outlet } from "../_libs/@tanstack/react-router+[...].mjs";
-//#region node_modules/.nitro/vite/services/ssr/assets/treatments-CXjwHqMc.js
-var import_jsx_runtime = require_jsx_runtime();
-function TreatmentsLayout() {
-	return /* @__PURE__ */ (0, import_jsx_runtime.jsx)(Outlet, {});
-}
-//#endregion
-export { TreatmentsLayout as component };
